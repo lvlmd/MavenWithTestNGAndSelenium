27412704e5a70c01d393305d37b66a6df1ad94d0
lvlmd <alujanschi@gmail.com>
2016-08-27 02:21:49 -0400

    added phantomjs test

---
+++ src/test/java/PhantomJS.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+
+public class PhantomJS {
+
+    public static void main(String[] args) {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "/usr/local/bin/phantomjs");
+
+        WebDriver driver = new PhantomJSDriver();
+        driver.get("http://google.com");
+        System.out.println("Page title = " + driver.getTitle());
+        driver.quit();
+
+    }
+}
