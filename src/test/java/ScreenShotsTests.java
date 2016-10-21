e36fc4fa7216da8c18e6c8952fd6c07d42361e52
lvlmd <alujanschi@gmail.com>
2016-10-21 13:43:54 -0400

    new commit!

--- src/test/java/ScreenShotsTests.java
+++ src/test/java/ScreenShotsTests.java
@@ -18,7 +18,7 @@ public class ScreenShotsTests {
         driver.get("https://USER:PASS@pfizerproitdev5.prod.acquia-sites.com/");
         driver.findElement(By.linkText("Log in")).click();
         driver.findElement(By.cssSelector("#capture_signIn_traditionalSignIn_emailAddress")).sendKeys("common_user");
-        driver.findElement(By.cssSelector("#capture_signIn_traditionalSignIn_password")).sendKeys("Pfizer@1234");
+        driver.findElement(By.cssSelector("#capture_signIn_traditionalSignIn_password")).sendKeys("PASS");
         driver.findElement(By.cssSelector("#capture_signIn_traditionalSignIn_signInButton")).click();
         ScreenShot((TakesScreenshot) driver);
         driver.close();
